09f8dd16bf2e25d48f87fda8e22842e86ecb92c3
SAFEERALI173 <safeerali1737@gmail.com>
2026-01-29 01:08:29 +0500

    added

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const app = express()
 const StudentRoute = require("./routes/student")
+const GradeRoute = require("./routes/grade")
+
 app.use(express.json())
 const connectDB = require("./config/db")
 
 app.use("/student",StudentRoute)
+app.use("/grade",GradeRoute)
 
 connectDB().then(()=>{
     app.listen(5000,()=>{
